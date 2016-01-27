792da5da7936a7d8124a72ab60b0ba118a285e3c
Andrey Barabash <illuminatedzp@gmail.com>
2016-01-27 11:30:56 +0200

    Added sign in & registration interface

--- app/src/main/java/com/monz/project_note/app/MainActivity.java
+++ app/src/main/java/com/monz/project_note/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.monz.project_note.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Андрей on 26.01.2016.
@@ -10,6 +11,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_layout);
+    }
+    public void signInClick(View v){
         setContentView(R.layout.activity_main);
+    }
+    public void registrationClick(View v){
+        setContentView(R.layout.registration_layout);
     }
 }
