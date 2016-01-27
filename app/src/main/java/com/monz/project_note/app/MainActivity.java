d60233880dc7542bb03f58117590d6ec020ffdbb
Andrey Barabash <illuminatedzp@gmail.com>
2016-01-27 16:03:18 +0200

    Added land layout for sign in and registration interface Added toolbar and menu interface

--- app/src/main/java/com/monz/project_note/app/MainActivity.java
+++ app/src/main/java/com/monz/project_note/app/MainActivity.java
@@ -1,20 +1,40 @@
 package com.monz.project_note.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
 
 /**
  * Created by Андрей on 26.01.2016.
  */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_layout);
     }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.titleText);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
     public void signInClick(View v){
         setContentView(R.layout.activity_main);
+        initToolbar();
     }
     public void registrationClick(View v){
         setContentView(R.layout.registration_layout);
